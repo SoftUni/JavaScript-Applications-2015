3fbe103de4a5c04cea120ed9306b76d9e1813468
VGGeorgiev <vggeorgiev@mail.bg>
2014-12-01 14:30:07 +0200

    Added underscore.js and Unit testing demos

--- 2. Cookies and Web Storages/scripts/storage-extensions.js
+++
@@ -1,7 +0,0 @@
-Storage.prototype.setObject = function setObject(key, obj) {
-    this.setItem(key, JSON.stringify(obj));
-};
-
-Storage.prototype.getObject = function getObject(key) {
-    return JSON.parse(this.getItem(key));
-};
